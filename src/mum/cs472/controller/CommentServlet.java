0fca2726ada2ddcd2c3ebd6b04326aacd24018f9
Jim2014 <jim_zhoujin@163.com>
2017-03-22 23:25:31 +0800

    fix bug

--- src/mum/cs472/controller/CommentServlet.java
+++ src/mum/cs472/controller/CommentServlet.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-import com.alibaba.fastjson.JSON;
-import com.google.gson.*;
+
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
